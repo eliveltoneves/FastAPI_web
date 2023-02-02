9e4988093c88a39492484f39aa075290c0faac1f
elivelton <neves_elivelton@yahoo.com.br>
2023-02-02 09:51:35 -0300

    primeiro commit

---
+++ core/configs.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+
+class Settings(BaseSettings):
+    DB_URL: str = 'postgresql+asyncpg://postgres:reuza1908@localhost:5432/startupAlcantara'
+    DBBaseModel = declarative_base()
+    TEMPLATES = Jinja2Templates(directory='templates')
+    MEDIA = Path('media')
+
+    class Config:
+        case_sensitive = True
+
+
+
+settings: Settings = Settings()
